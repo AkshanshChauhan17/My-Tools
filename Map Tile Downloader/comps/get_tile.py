ff383616237c624498dd4d80b04984f3f6abb63a
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-08 21:10:15 +0530

    build working model

--- Map Tile Downloader/comps/get_tile.py
+++ Map Tile Downloader/comps/get_tile.py
@@ -2,13 +2,13 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-def display_image_from_url(image_url, index):
+def display_image_from_url(image_url, img_name):
     try:
-        print('image saved ->', index)
+        print('image saved ->', img_name)
         response = requests.get(image_url)
         response.raise_for_status()
 
         img = Image.open(BytesIO(response.content))
-        img.save('comps/imgs/' + str(index) + '.png')
+        img.save('comps/imgs/' + img_name)
     except Exception as e:
         print(f"Error: {e}")
--- Map Tile Downloader/main.py
+++ Map Tile Downloader/main.py
@@ -7,10 +7,9 @@ else:
     x_inp = int(sys.argv[1])
     y_inp = int(sys.argv[2])
     z_inp = int(sys.argv[3])
-    index = 0
-    for x in range(2):
-        for y in range(2):
-            index = index + 1
+    for y in range(y_inp):
+        for x in range(x_inp):
             x = x + x_inp
             y = y + y_inp
-            get_tile.display_image_from_url(f"https://mts1.google.com/vt/lyrs=m@186112443&hl=x-local&src=app&x={x}&y={y}&z={z_inp}", index)
+            file_name = f"{x}.{y}.{z_inp}.png"
+            get_tile.display_image_from_url(f"https://mts1.google.com/vt/lyrs=m@186112443&hl=x-local&src=app&x={x}&y={y}&z={z_inp}", file_name)
