c265d3d490d80a0d4676fea19720a22d67578c4b
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-08 21:48:04 +0530

    fixed some issue in map downloader

--- Map Tile Downloader/main.py
+++ Map Tile Downloader/main.py
@@ -1,15 +1,16 @@
 from comps import get_tile
 import sys
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     print("Null")
 else:
     x_inp = int(sys.argv[1])
     y_inp = int(sys.argv[2])
     z_inp = int(sys.argv[3])
-    for y in range(y_inp):
-        for x in range(x_inp):
-            x = x + x_inp
-            y = y + y_inp
-            file_name = f"{x}.{y}.{z_inp}.png"
-            get_tile.display_image_from_url(f"https://mts1.google.com/vt/lyrs=m@186112443&hl=x-local&src=app&x={x}&y={y}&z={z_inp}", file_name)
+    l_inp = int(sys.argv[4])
+    for y in range(0, l_inp):
+        for x in range(0, l_inp):
+            file_name = f"{x_inp}.{y_inp}.{z_inp}.png"
+            get_tile.display_image_from_url(f"https://mts1.google.com/vt/lyrs=m@186112443&hl=x-local&src=app&x={x_inp}&y={y_inp}&z={z_inp}", file_name)
+            x_inp = x_inp + 1
+        y_inp = y_inp + 1
