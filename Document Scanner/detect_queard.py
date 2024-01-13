ad76062e4e305cd93f2080ff023e9eb736f89fa3
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-13 19:59:22 +0530

    changes in detection

--- Document Scanner/detect_queard.py
+++ Document Scanner/detect_queard.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
+import os
 
-img = cv2.imread('./chess.png')
-
+root = os.getcwd()
+print(root)
+imgPath = os.path.join(root, '/chess.png')
+img = cv2.imread(imgPath)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 corners = cv2.goodFeaturesToTrack(gray, maxCorners=4, qualityLevel=0.01, minDistance=10)
-
-corners = np.int0(corners)
 
 for corner in corners:
     x, y = corner.ravel()
