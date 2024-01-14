031e9ac151ec19f0524025457248febcdee5b09c
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-14 23:39:07 +0530

    added plt

--- Document Scanner/detect_queard.py
+++ Document Scanner/detect_queard.py
@@ -1,19 +1,14 @@
 import cv2
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 
-root = os.getcwd()
-print(root)
-imgPath = os.path.join(root, '/chess.png')
-img = cv2.imread(imgPath)
+img = cv2.imread('chess.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 corners = cv2.goodFeaturesToTrack(gray, maxCorners=4, qualityLevel=0.01, minDistance=10)
 
 for corner in corners:
     x, y = corner.ravel()
-    cv2.circle(img, (x, y), 3, 255, 1)
+    cv2.circle(img, (x, y), 3, 255, -1)
 
-
-cv2.imshow('corners', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(img), plt.show()
