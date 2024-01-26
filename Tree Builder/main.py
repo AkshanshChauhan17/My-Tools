c4a04488d8f7096089db8520122ab0b0b414b88e
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-27 01:52:51 +0530

    added tree making

---
+++ Tree Builder/main.py
@@ -0,0 +1,16 @@
+def divider(x):
+    return int(x/2)
+
+def splitter(x):
+    return int(x*2)
+
+def build(x):
+    step = 0
+    while x>0:
+        step = step + 1
+        x = divider(x)
+        for i in range(splitter(step)):
+            print(x, " ", end="")
+        print("")
+
+build(1000)
