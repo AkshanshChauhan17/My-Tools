9972529e62d8fa562c3d067944636c3300c9bacb
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-27 20:38:45 +0530

    changes

--- Pdf Reader/main.py
+++ Pdf Reader/main.py
@@ -1,34 +1,10 @@
-import requests
-import PyPDF2
+from PyPDF2 import PdfReader 
 
-def read_pdf_from_web(pdf_url):
-    try:
-        response = requests.get(pdf_url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        
-        with open('downloaded_pdf.pdf', 'wb') as file:
-            file.write(response.content)
-        
-        return read_pdf('downloaded_pdf.pdf')
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading PDF from {pdf_url}: {e}")
-        return None
+reader = PdfReader('example.pdf') 
+ 
+print(len(reader.pages)) 
 
-def read_pdf(file_path):
-    with open(file_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        num_pages = pdf_reader.numPages
+page = reader.pages[0] 
 
-        text = ''
-        for page_num in range(num_pages):
-            page = pdf_reader.getPage(page_num)
-            text += page.extractText()
-
-    return text
-
-# Example usage:
-pdf_url = "https://online.uktech.ac.in/ums/Print/DownloadPDFPromotionApplicatinForm?StudentPromotionId=7"  # Replace with the actual URL of the PDF
-pdf_text = read_pdf_from_web(pdf_url)
-
-if pdf_text:
-    print(pdf_text)
+text = page.extract_text().split("\n").index(": Student's Name")
+print(text)
