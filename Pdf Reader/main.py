127369599ae47fdb0dddb479c1cb091c2e59edc4
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-27 20:48:11 +0530

    changes

--- Pdf Reader/main.py
+++ Pdf Reader/main.py
@@ -1,10 +1,15 @@
 from PyPDF2 import PdfReader 
+import glob
 
-reader = PdfReader('example.pdf') 
- 
-print(len(reader.pages)) 
+pdf_list = glob.glob("*.pdf")
 
-page = reader.pages[0] 
+for pdf in pdf_list:
+    reader = PdfReader(pdf) 
+    page_length = len(reader.pages) 
 
-text = page.extract_text().split("\n").index(": Student's Name")
-print(text)
+    if page_length == 0:
+        print("__null__")
+    else:
+        page = reader.pages[0] 
+        text = page.extract_text().split("\n")[11]
+        print(text)
